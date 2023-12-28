30a259ab25976b04521fb8d29b77875214278e57
Dzianis Staliarou <Dzianis_Staliarou@epam.com>
2023-12-28 10:27:55 +0400

    Add a RabbitMQ sender.

---
+++ Message Brokers/RabbitMQSender/Actions/SendAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQSender.Actions
+{
+    public static class SendAction
+    {
+        public static void Start(IModel channel)
+        {
+            channel.QueueDeclare(
+                queue: "common",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            for (int i = 0; i < 10; i++)
+            {
+                string message = $"Data - {i}";
+
+                Send(channel, message);
+                Console.WriteLine($"[i]: Sent - {message}");
+            }
+
+            static void Send(IModel channel, string message)
+            {
+                byte[] body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: "common",
+                    basicProperties: null,
+                    body: body);
+            }
+        }
+    }
+}
---
+++ Message Brokers/RabbitMQSender/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using RabbitMQSender.Actions;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+SendAction.Start(channel);
+
+Console.ReadLine();
