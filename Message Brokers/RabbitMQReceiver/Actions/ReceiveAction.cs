1a7c1d9cfb47010f882c61921c77ff01c2c92270
Dzianis Staliarou <Dzianis_Staliarou@epam.com>
2023-12-28 10:28:28 +0400

    Add a RabbitMQ receiver.

---
+++ Message Brokers/RabbitMQReceiver/Actions/ReceiveAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQReceiver.Actions
+{
+    public static class ReceiveAction
+    {
+        public static void Start(IModel channel)
+        {
+            channel.QueueDeclare(
+                queue: "common",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            EventingBasicConsumer consumer = new(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine($"[i]: Received - {message}");
+            };
+
+            channel.BasicConsume(
+                queue: "common",
+                autoAck: true,
+                consumer);
+        }
+    }
+}
---
+++ Message Brokers/RabbitMQReceiver/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using RabbitMQReceiver.Actions;
+
+ConnectionFactory factory = new()
+{
+    HostName = "localhost",
+};
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+ReceiveAction.Start(channel);
+
+Console.ReadLine();
